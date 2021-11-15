3e868652d02da06aee8881836849c51469536edf
caichunyu <caichunyu0@gmail.com>
2021-11-15 14:46:25 +0800

    refactor: hero model

--- server/models/Category.js
+++ server/models/Category.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 
 const schema = new mongoose.Schema({
   name: {type: String},
-  parent: {type: mongoose.SchemaTypes.ObjectId,ref:'Category'},
+  parent: {type: mongoose.SchemaTypes.ObjectId,ref:'Category'}, //关联查询
 
 })
 
