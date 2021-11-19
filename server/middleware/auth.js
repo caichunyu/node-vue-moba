2c6124fee116ec908dc0ed2b0212dd50d8fe573c
caichunyu <caichunyu0@gmail.com>
2021-11-19 14:05:26 +0800

    fix: fix app is undefind

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,11 +1,13 @@
-const assert = require("http-assert");
-const jwt = require("jsonwebtoken");
-const AdminUser = require("../models/AdminUser");
 module.exports = options =>{
+
+  const assert = require("http-assert");
+  const jwt = require("jsonwebtoken");
+  const AdminUser = require("../models/AdminUser");
+
   return async (req, res, next) => {
     const token = String(req.headers.authorization || '').split(' ').pop()
     assert(token, 401, '请 token') //捕获异常
-    const {id} = jwt.verify(token, app.get('secret'))
+    const {id} = jwt.verify(token, req.app.get('secret'))
     assert(id, 401, '无效 token') //捕获异常
     req.user = await AdminUser.findById(id)
     assert(req.user, 401, '请先登录') //捕获异常
