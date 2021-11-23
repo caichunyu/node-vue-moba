bcc0fcb0b65784b2094b368b15c91e16c4ab380e
caichunyu <caichunyu0@gmail.com>
2021-11-23 22:21:30 +0800

    feat: swiper

--- web/src/main.js
+++ web/src/main.js
@@ -1,9 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-import  './style.scss'
+import  './assets/scss/style.scss'
 import router from './router'
 
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'swiper/css/swiper.css'
+
+Vue.use(VueAwesomeSwiper)
 Vue.config.productionTip = false
 
 new Vue({
--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,5 +1,24 @@
 <template>
   <div class="home">
+    <swiper>
+      <swiper-slide>
+        <img class="w-100" src="../assets/images/856ed2a1d233b5d22e3ff410b1fb4602.jpeg">
+      </swiper-slide>
+      <swiper-slide>
+        <img class="w-100" src="../assets/images/b945919f9ab33873d25b06cf2e2e1169.jpeg">
+      </swiper-slide>
+      <swiper-slide>
+        <img class="w-100" src="../assets/images/c136017e579e534d3b12636c2b70a13a.jpeg">
+      </swiper-slide>
+      <div class="swiper-pagination pagination-home text-right px-3 pb-1" slot="pagination"></div>
+    </swiper>
+<!--    end of swiper-->
+    <div class="nav-icons bg-white mt-3 d-flex flex-wrap text-center text-dark-1">
+      <div class="nav-item p-3" v-for="n in 10" :key="n">
+        <i class="sprite sprite-news"></i>
+        <div >爆料站</div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -8,5 +27,36 @@
 
 export default {
   name: 'Home',
+  data() {
+    return {
+      swiperOption: {
+        pagination: {
+          el: '.pagination-home'
+        }
+      }
+    }
+  }
 }
 </script>
+<style lang="scss">
+@import "../assets/scss/variables";
+
+.pagination-home {
+  .swiper-pagination-bullet {
+    opacity: 1;
+    border-radius: 0.154rem;
+    background:#fff;
+    //background: map-get($colors, 'white');
+
+    &.swiper-pagination-bullet-active {
+      background: #4b67af;
+      //background: map-get($colors, 'info');
+    }
+  }
+}
+.nav-icons{
+.nav-item{
+  width: 25%;
+}
+}
+</style>
