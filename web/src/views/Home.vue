306ef7cf5b2475723e5f8ad5b9d69544ff9032de
caichunyu <caichunyu0@gmail.com>
2021-11-24 14:31:00 +0800

    feat: sprite icon

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <swiper>
+    <swiper :options="swiperOption">
       <swiper-slide>
         <img class="w-100" src="../assets/images/856ed2a1d233b5d22e3ff410b1fb4602.jpeg">
       </swiper-slide>
@@ -12,11 +12,17 @@
       </swiper-slide>
       <div class="swiper-pagination pagination-home text-right px-3 pb-1" slot="pagination"></div>
     </swiper>
-<!--    end of swiper-->
-    <div class="nav-icons bg-white mt-3 d-flex flex-wrap text-center text-dark-1">
-      <div class="nav-item p-3" v-for="n in 10" :key="n">
-        <i class="sprite sprite-news"></i>
-        <div >爆料站</div>
+    <!--    end of swiper-->
+    <div class="nav-icons bg-white mt-3 pt-3  text-center text-dark-1">
+      <div class="d-flex flex-wrap">
+        <div class="nav-item mb-3 " v-for="n in 10" :key="n">
+          <i class="sprite sprite-news"></i>
+          <div class="py-2">爆料站</div>
+        </div>
+      </div>
+      <div class="bg-light py-2 fs-sm">
+        <i class="sprite sprite-arrow mr-1"></i>
+        <span>收起</span>
       </div>
     </div>
   </div>
@@ -45,18 +51,20 @@ export default {
   .swiper-pagination-bullet {
     opacity: 1;
     border-radius: 0.154rem;
-    background:#fff;
-    //background: map-get($colors, 'white');
+    background: map-get($colors, 'white');
 
     &.swiper-pagination-bullet-active {
-      background: #4b67af;
-      //background: map-get($colors, 'info');
+      background: map-get($colors, 'info');
     }
   }
 }
-.nav-icons{
-.nav-item{
-  width: 25%;
-}
+
+.nav-icons {
+  border-top: 1px solid $border-color;
+  border-bottom: 1px solid $border-color;
+  .nav-item {
+    width: 25%;
+    border-right: 1px solid $border-color;
+  }
 }
 </style>
